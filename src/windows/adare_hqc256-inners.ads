bbba67491702dff110606508f1832140f049381f
Daniel Norte de Moraes <danieagle@zohomail.com>
2024-08-15 16:51:50 -0300

    Important tipo fix. Thanks God and Mary and Cia!! Enjoy!! :-D

--- src/windows/adare_hqc256-inners.ads
+++ src/windows/adare_hqc256-inners.ads
@@ -9,7 +9,7 @@ package Adare_Hqc256.inners with Pure is
      private_key_i  :   System.Address  -- out
     ) return Interfaces.C.int
 
-    with Import => True, Convention => StdCall External_Name => "PQCLEAN_HQC256_CLEAN_crypto_kem_keypair";
+    with Import => True, Convention => StdCall, External_Name => "PQCLEAN_HQC256_CLEAN_crypto_kem_keypair";
 
   function internal_hqc_256_kem_enc
     (ciphertext_i       :   System.Address; -- out
@@ -17,7 +17,7 @@ package Adare_Hqc256.inners with Pure is
      public_key_i       :   System.Address  -- in
     ) return Interfaces.C.int
 
-    with Import => True, Convention => StdCall External_Name => "PQCLEAN_HQC256_CLEAN_crypto_kem_enc";
+    with Import => True, Convention => StdCall, External_Name => "PQCLEAN_HQC256_CLEAN_crypto_kem_enc";
 
   function internal_hqc_256_kem_dec
     (shared_secret_i    :   System.Address; -- out
@@ -25,6 +25,6 @@ package Adare_Hqc256.inners with Pure is
      secret_key_i       :   System.Address  -- in
     ) return Interfaces.C.int
 
-    with Import => True, Convention => StdCall External_Name => "PQCLEAN_HQC256_CLEAN_crypto_kem_dec";
+    with Import => True, Convention => StdCall, External_Name => "PQCLEAN_HQC256_CLEAN_crypto_kem_dec";
 
 end Adare_Hqc256.inners;
